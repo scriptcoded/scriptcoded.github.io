3fb5867b1dd20096b402b74bad8758f16bd37f6f
Script_Coded <script_coded@hotmail.com>
2015-10-24 18:40:29 +0200

    Fixed sidebar

--- MESIT/js/tapSidebar.js
+++ MESIT/js/tapSidebar.js
@@ -1,6 +1,15 @@
 $().ready(function() {
-    $("#tapMenuButton").click(function() {
-        //body.animate({ right: '200px' });
-        alert("Ey!");
-    });
+	$("#tapMenuButton").click(function() {
+		if ($("#tapSidebar").css("right") == "-200px") {
+			$("#tapSidebar").animate({ right: "0px" });
+			$("#tapHeader").animate({ right: "200px" });
+			$("#tapSidebar").show()
+		}
+		else if ($("#tapSidebar").css("right") == "0px") {
+			$("#tapSidebar").animate({ right: "-200px" });
+			$("#tapHeader").animate({ right: "0px" }, function () {
+				$("#tapSidebar").hide();
+			});
+		}
+	});
 }); 
